6221b25e6187331b67ff17fb9f4a893810fe2624
VenkataSarma <166056655+VenkataSarma@users.noreply.github.com>
2025-08-18 23:34:59 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(process.env.PORT, () => console.log("Server running...")))
+  .catch(err => console.log(err));
